edfcfa9bc1d56a230b0296d922f8d57b2bbace73
Pias Hossain <piashossain009@gmail.com>
2021-02-28 20:44:36 +0600

    Header Added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import fakeData from '../../fakeData';
+
+
+
+const Shop = () => {
+  console.log(fakeData);
+    return (
+        <div>
+            <h1>this is shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
